648634161cf71ccaff1aa544ecdfd4161e91dda9
decademo <893715244@qq.com>
2020-10-13 20:37:44 +0800

    Initial commit

---
+++ fourseason02/src/components/LoginPage.vue
@@ -0,0 +1,26 @@
+<template>
+  <el-container>
+    <!-- 登录界面 -->
+    <el-row>
+      <el-col :span="6">
+        <el-row>登录</el-row>
+        <el-row>
+          <el-input  />
+        </el-row>
+        <el-row>
+          <el-input  />
+        </el-row>
+      </el-col>
+    </el-row>
+  </el-container>
+</template>
+
+<script>
+    export default {
+        name: "LoginPage"
+    }
+</script>
+
+<style scoped>
+
+</style>
